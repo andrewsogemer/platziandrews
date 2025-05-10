e1385df34ce8cb9b21629984460c5146507fa2df
Carlos Martinez <carlosmartinez@Carloss-MacBook-Air.local>
2025-05-09 21:00:05 -0500

    operadores_logicos

---
+++ Fundamentos_js/Operadores_Logicos/app.js
@@ -0,0 +1,12 @@
+/*
+Operadores Logicos
+
+1. && si => este valor && este valor pasa esto
+2. || or => si este valor OR este valor pasa esto
+3. ! not => 
+
+*/
+
+const a = 10;
+const b = 20;
+const c = '10'
