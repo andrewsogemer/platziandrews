04069e6fb85ea7c1652bb36715f4ec178852eee2
Carlos Martinez <carlosmartinez@Carloss-MacBook-Air.local>
2025-05-09 22:01:59 -0500

    condicionales_3

---
+++ Fundamentos_js/Estructuras_Condicionales/app.js
@@ -0,0 +1,46 @@
+/*
+
+Estructuras de CONTROL
+
+1. if
+
+if(let === algo){
+codigo
+}else{
+    codigo
+}
+
+2. if else
+
+if (let === algo){
+    codigo
+}else if{
+    codigo
+}else{
+    codigo
+}
+
+let nombre = 'Nico';
+if (nombre === 'Diego'){
+    console.log('Hola Diego')
+} else{
+    console.log('Nombre no Encontrado')
+}
+
+
+
+*/
+
+let nombre = 'Diego';
+
+if(nombre === 'Diego'){
+    console.log('Hola Diego')
+}else if(nombre === 'Nico'){
+    console.log('Hola Nico')
+}else{
+    console.log('Nombre no encontrado')
+}
+
+
+
+
