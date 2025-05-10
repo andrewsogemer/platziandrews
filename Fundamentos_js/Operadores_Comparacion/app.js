23af32c217bfe22002b6596d39a78ec42a19a365
Carlos Martinez <carlosmartinez@Carloss-MacBook-Air.local>
2025-05-09 20:58:55 -0500

    Operadores

---
+++ Fundamentos_js/Operadores_Comparacion/app.js
@@ -0,0 +1,24 @@
+/* 
+Operadores de Comparacion
+
+1. == compara si algo es igual a otra cosa
+2. === compara de forma estricta algo con otra cosa y compara en dos temas en valor y tipo de dato
+3. != es diferente de un valor a otro valor
+4. !== compara diferentes valores de manera extricta tanto en valor y tipo de dato
+5. > Mayor que
+6. < Menor que
+7. >= Mayor o igual
+8. <= Menor o Igual
+9. 
+
+*/
+
+// Ejemplos
+
+const a = 10;
+const b = 20;
+const c = '10';
+
+a == b;
+
+
